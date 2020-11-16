5cc5e090442b2729f077e916e33a525edc547711
Mana <manakouwenberg@gmail.com>
2020-11-16 11:23:25 +0100

    Added grid movement and turn based gameplay handling.

---
+++ Assets/Scripts/CharacterControllers/TurnBased/TestTurnActors.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTurnActors : TurnActor
+{
+    Rigidbody rb;
+    public int amountOfTurnsTilDeath;
+
+    protected override void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        base.Start();
+
+    }
+    public override IEnumerator Act()
+    {
+        rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        initiative = TurnManager.Instance.GetLastActorInitiative() + 1;
+        amountOfTurnsTilDeath -= 1;
+        if(amountOfTurnsTilDeath < 1) { Destroy(gameObject); yield break; }
+
+        yield return new WaitForSeconds(3.0f);
+    }
+}
---
+++ Assets/Scripts/CharacterControllers/TurnBased/TurnPlayerActor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnPlayerActor : TurnActor
+{
+    public override IEnumerator Act()
+    {
+        throw new System.NotImplementedException();
+    }
+}
